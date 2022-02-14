103a4d24821ce3e25a6153a66919411570079016
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-14 15:49:55 -0800

    File tree created and files are linked

---
+++ src/container/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from '../../presentational/List/List';
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      ShoppingCart
+      <List />
+    </div>
+  );
+};
+
+export default ShoppingCart;
---
+++ src/presentational/List/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from '../ListItem/ListItem';
+
+const List = () => {
+  return (
+    <div>
+      List
+      <ListItem />
+    </div>
+  );
+};
+
+export default List;
