39614870c274444db2ded1858d3d27303c306d52
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-14 16:01:04 -0800

    added onSubmit to form and type to button

--- src/presentational/Controls/Controls.js
+++ src/presentational/Controls/Controls.js
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 const Controls = () => {
   const [add, setAdd] = useState('');
 
+  const handleAdd = () => {};
+
   return (
     <>
       Controls
-      <form>
+      <form onSubmit={handleAdd}>
         <input
           type="text"
           placeholder="add an item"
           value={add}
           onChange={(e) => setAdd(e.target.value)}
         />
-        <button>Add this item</button>
+        <button type="submit">Add this item</button>
       </form>
     </>
   );
